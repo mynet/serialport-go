63196176e26e8ae324a48156d51a1566f5cace2d
D. Chiang <dc@rydc.io>
2019-05-03 08:43:05 +0800

    fix loop

--- serialport.go
+++ serialport.go
@@ -9,7 +9,7 @@ import (
     "fmt"
     "strings"
     "github.com/tarm/serial"
-    // "github.com/gorilla/websocket"
+    "github.com/gorilla/websocket"
 )
 
 func main() {
@@ -28,28 +28,28 @@ func main() {
     fmt.Println("Connected to " + string(port))
 
     buf := make([]byte, 128)
-    t1 := time.NewTicker(time.Millisecond * 500)
-    for t := range t1.C {
-        fmt.Println("Ticker start @ ", t)
-        n, err := s.Read(buf)
-        if err != nil {
-            log.Fatal(err)
-        }
-        str := string(buf)
-        log.Printf("%q", buf[:n])
-        if r.MatchString(str) {
-            matched_string := r.FindString(str)
-            fmt.Println(matched_string)
-            // websocket_connection, _, err := websocket.DefaultDialer.Dial("ws://127.0.0.1:8899/serialport", nil)
-            // if err != nil {
-            //     log.Fatal("dial:", err)
-            // }
-            // defer websocket_connection.Close()
-            // err = websocket_connection.WriteMessage(websocket.TextMessage, []byte(matched_string))
-            // if err != nil {
-            //     log.Println(err)
-            //     return
-            // }
+    for {
+        select {
+            case <-time.After(500 * time.Millisecond):
+                n, err := s.Read(buf)
+                if err != nil {
+                    log.Fatal(err)
+                }
+                str := string(buf)
+                log.Printf("%q", buf[:n])
+                if r.MatchString(str) {
+                    matched_string := r.FindString(str)
+                    websocket_connection, _, err := websocket.DefaultDialer.Dial("ws://127.0.0.1:8899/serialport", nil)
+                    if err != nil {
+                        log.Fatal("dial:", err)
+                    }
+                    defer websocket_connection.Close()
+                    err = websocket_connection.WriteMessage(websocket.TextMessage, []byte(matched_string))
+                    if err != nil {
+                        log.Println(err)
+                        return
+                    }
+                }
         }
     }
 }
